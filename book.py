7da5ce8af59a3372e062cdcf60494ede1fbeb301
clafish <andrew.korzh2008@gmail.com>
2026-01-16 16:45:41 +0200

    Book class creation

---
+++ book.py
@@ -0,0 +1,8 @@
+class Book:
+    def __init__(self, name, year, author):
+        self.name = name
+        self.year = year
+        self.author = author
+
+    # def __eq__(self, other):
+    #     return self.name == other.name and self.author == other.author and self.year == other.year
